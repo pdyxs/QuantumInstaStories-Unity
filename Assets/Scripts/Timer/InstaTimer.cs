eecdd371e91b056c98e211c11c57c6ea9c3a505a
Paul Sztajer <talk@pdyxs.wtf>
2019-09-13 14:25:57 +0200

    starting and getting timers working

---
+++ Assets/Scripts/Timer/InstaTimer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstaTimer : MonoBehaviour
+{
+
+	public RectTransform myTransform;
+
+	public void ChangeTimer(float amount)
+	{
+		myTransform.anchorMax = new Vector2(amount, myTransform.anchorMax.y);
+	}
+}
---
+++ Assets/Scripts/Timer/InstaTimers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InstaTimers : MonoBehaviour
+{
+
+	public float totalTime = 10;
+
+	private float timeSoFar = 0;
+
+	public bool paused = false;
+
+	public InstaTimer[] timers;
+
+	private int currentSelection = 0;
+
+	private void Start()
+	{
+		timers = GetComponentsInChildren<InstaTimer>().ToArray();
+	}
+
+	private void Update()
+	{
+		timeSoFar += Time.deltaTime;
+		if (timeSoFar < totalTime)
+		{
+			timers[currentSelection].ChangeTimer(timeSoFar / totalTime);
+		}
+		else 
+		{
+			timers[currentSelection].ChangeTimer(1);
+			if (currentSelection + 1 < timers.Length)
+			{
+				currentSelection++;
+				timeSoFar = 0;
+			}
+		}
+	}
+
+	public void Pause()
+	{
+		paused = true;
+	}
+
+	public void Unpause()
+	{
+		paused = true;
+	}
+
+	public void ResetTimer()
+	{
+		timeSoFar = 0;
+	}
+}
